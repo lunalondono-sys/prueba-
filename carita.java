f3af1c52b2dfa829ff764a59243bc5fc65a289ab
lunalondono-sys <luna.londono@cun.edu.co>
2025-08-12 12:45:44 -0500

    Add files via upload

---
+++ carita.java
@@ -0,0 +1,14 @@
+
+public class carita {
+  public static void main (String[] args ) {
+    System.out.println("  @@@@@@@ ");
+    System.out.println(" @| O O |@ ");
+    System.out.println(" (|  ^  |) ");
+    System.out.println("  | [_] | ");
+    System.out.println("  +-----+ ");
+  }
+}
+ 
+
+
+ 
